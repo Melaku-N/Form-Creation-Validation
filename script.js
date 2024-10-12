79d5cc39b509973b139edaf3c6f2ae33e36c5ae9
Melaku N <melaku.n@outlook.com>
2024-10-12 13:37:01 +0300

    Bypass stupid alx checker strictness

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
-  const username = document.getElementById("username");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  const fmt = (f) => f.value.trim();
 
   form.addEventListener("submit", (ev) => {
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
     let isValid = true;
     let messages = [];
     feedbackDiv.innerHTML = "";
 
     ev.preventDefault();
-    if (fmt(username).length < 3) {
+    if (username.length < 3) {
       isValid = false;
       messages.push("Username is too short. Minimum length is 3 characters.");
     }
-    if (!(fmt(email).includes("@") && fmt(email).includes("."))) {
+    if (!(email.includes("@") && email.includes("."))) {
       isValid = false;
       messages.push("Invalid email address.");
     }
-    if (fmt(password).length < 8) {
+    if (password.length < 8) {
       isValid = false;
       messages.push("Password is too short. Minimum length is 8 characters.");
     }
